10ad67c41f6d7a21283e4380b3ed9137bd18e794
Saksham Yadav <infosakshamyadav@gmail.com>
2025-03-22 02:56:12 -0500

    feat:user customizable navbar

--- cartcraze/src/components/NavBar.jsx
+++ cartcraze/src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Dropdown, Nav, Navbar } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 const NavBar = () => {
+  const { isLoggedIn, username } = useSelector((state) => state.auth);
+
   return (
     <>
-      <Navbar bg="primary" data-bs-theme="dark">
+      <Navbar bg="warning" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="/" className="fs-4">
             CartCraze
@@ -17,11 +19,34 @@ const NavBar = () => {
           </Nav>
           <Nav>
             <Nav.Link className="text-light fs-5" href="/">
-              Login
+              {isLoggedIn ? <></> : <>Login</>}
             </Nav.Link>
-            <Nav.Link className="fs-5">
-              <i className="fa-solid fa-cart-shopping"></i>
-            </Nav.Link>
+
+            {isLoggedIn ? (
+              <>
+                <Dropdown>
+                  <Dropdown.Toggle variant="info">{username}</Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>
+                      <i className="fa-solid fa-user"></i> Profile
+                    </Dropdown.Item>
+                    <Dropdown.Item href="/cart">
+                      <i className="fa-solid fa-cart-shopping"></i> Cart
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <i className="fa-solid fa-right-from-bracket"></i> Logout
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <i className="fa-solid fa-gear"></i> Settings
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </>
+            ) : (
+              <></>
+            )}
+
+            {/* <Button onClick={handle}> click</Button> */}
           </Nav>
         </Container>
       </Navbar>
